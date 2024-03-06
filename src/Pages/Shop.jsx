b6e5cec501eb16df4cc2fe95badc6f2c20f201c6
minatisleeping <minatt2002@gmail.com>
2024-03-06 16:13:05 +0700

    done feature3

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   )
 }
