374bbbba25b9b9cd36e0330088f0b6ca79ede0f5
minatisleeping <minatt2002@gmail.com>
2024-03-20 08:21:59 +0700

    Config MUI theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
-import SvgIcon from '@mui/material/SvgIcon'
-import Stack from '@mui/material/Stack'
-
-function HomeIcon(props) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
-  )
-}
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <div>minat is comminggg</div>
+
+      <Typography variant='body2' color='text.primary'>test Typography</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
@@ -24,15 +19,14 @@ function App() {
       <AccessAlarmIcon/>
       <ThreeDRotation/>
 
-      <Stack direction="row" spacing={3}>
-        <HomeIcon />
-        <HomeIcon color="primary" />
-        <HomeIcon color="secondary" />
-        <HomeIcon color="success" />
-        <HomeIcon color="action" />
-        <HomeIcon color="disabled" />
-        <HomeIcon sx={{ color: pink[100] }} />
-      </Stack>
+      <HomeIcon />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[100] }} />
+
 
     </>
   )
