aa8b9296f26f785a52cb0540abbe0ca6ca7eace4
minatisleeping <minatt2002@gmail.com>
2024-03-29 11:54:44 +0700

    Code app bar(header) p3

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -16,13 +16,13 @@ function ModeSelect() {
   }
 
   return (
-    <FormControl size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+    <FormControl size='small' sx={{ minWidth: 120 }}>
+      <InputLabel id='label-select-dark-light-mode'>Mode</InputLabel>
       <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
+        labelId='label-select-dark-light-mode'
+        id='select-dark-light-mode'
         value={mode}
-        label="Mode"
+        label='Mode'
         onChange={handleChange}
       >
         <MenuItem value='light'>
