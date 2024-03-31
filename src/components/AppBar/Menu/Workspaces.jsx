fdf7a3beeeaac51b8092cab82812bba16fcf112e
minatisleeping <minatt2002@gmail.com>
2024-03-31 16:58:36 +0700

    Refactor theme app bar

--- src/components/AppBar/Menu/Workspaces.jsx
+++ src/components/AppBar/Menu/Workspaces.jsx
@@ -26,6 +26,7 @@ function Workspaces() {
   return (
     <Box>
       <Button
+        sx={{ color: 'white' }}
         id='basic-button-workspaces'
         aria-controls={open ? 'basic-menu-workspaces' : undefined}
         aria-haspopup='true'
