ca5a7d7c1f270f920cd338e9fee857736487c42f
minatisleeping <minatt2002@gmail.com>
2024-04-12 14:43:29 +0700

    update UI/UX and show right message error when call api boardId

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
 import Box from '@mui/material/Box'
 import Column from './Column/Column'
 import { Button } from '@mui/material'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
-
+import TextField from '@mui/material/TextField'
 import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
+import CloseIcon from '@mui/icons-material/Close'
 
 function ListColumns({ columns }) {
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false)
+  const toggleOpenNewColumnForm = () => setOpenNewColumnForm(!openNewColumnForm)
+
+  const [newColumnTitle, setNewColumnTitle] = useState('')
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      console.error(' Please enter Column title!')
+      return
+    }
+    // console.log('🚀 ~ ListColumns ~ newColumnTitle:', newColumnTitle)
+    // Gọi API ở đây..
+
+    // Đóng trạng thái thêm Column mới & clear input
+    toggleOpenNewColumnForm()
+    setNewColumnTitle('')
+  }
+
   /*
     Thằng SortableContext yêu cầu items là một mảng dạng ['id-1', 'id-2'] chứ k phải [{id: 'id-1'}, {id: 'id-2'}]
     Nếu k đúng thì vẫn kéo thả được nhưng k có animation
@@ -24,27 +43,82 @@ function ListColumns({ columns }) {
         {columns?.map(column => <Column key={column?._id} column={column} />)}
 
         {/* Box Add New Column */}
-        <Box sx={{
-          minWidth: '200px',
-          maxWidth: '200px',
-          mx: 2,
-          borderRadius: '6px',
-          height: 'fit-content',
-          bgcolor: '#ffffff3d'
-        }}>
-          <Button
-            startIcon={<NoteAddIcon />}
-            sx={{
-              color: 'white',
-              width: '100%',
-              justifyContent: 'flex-start',
-              pl: 2.5,
-              py: 1
-            }}
-          >
-            Add new column
-          </Button>
-        </Box>
+        {!openNewColumnForm
+          ? <Box onClick={toggleOpenNewColumnForm} sx={{
+            minWidth: '250px',
+            maxWidth: '250px',
+            mx: 2,
+            borderRadius: '6px',
+            height: 'fit-content',
+            bgcolor: '#ffffff3d'
+          }}>
+            <Button
+              startIcon={<NoteAddIcon />}
+              sx={{
+                color: 'white',
+                width: '100%',
+                justifyContent: 'flex-start',
+                pl: 2.5,
+                py: 1
+              }}
+            >
+              Add new column
+            </Button>
+          </Box>
+          : <Box sx={{
+            minWidth: '250px',
+            maxWidth: '250px',
+            mx: 2,
+            p: 1,
+            borderRadius: '6px',
+            height: 'fit-content',
+            bgcolor: '#ffffff3d',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 1
+          }}>
+            <TextField
+              label='Enter column title..'
+              type='text'
+              size='small'
+              variant='outlined'
+              autoFocus
+              value={newColumnTitle}
+              onChange={e => setNewColumnTitle(e.target.value)}
+              sx={{
+                '& label': { color: 'white' },
+                '& input': { color: 'white' },
+                '& label.Mui-focused': { color: 'white' },
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: 'white' },
+                  '&:hover fieldset': { borderColor: 'white' },
+                  '&:Mui-focused fieldset': { borderColor: 'white' }
+                }
+              }}
+            />
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Button
+                onClick={addNewColumn}
+                variant='contained' color='success' size='small'
+                sx={{
+                  boxShadow: 'none',
+                  border: '0.5px solid',
+                  borderColor: theme => theme.palette.success.main,
+                  '&:hover': { bgcolor: theme => theme.palette.success.main }
+                }}
+              >Add New Column</Button>
+              <CloseIcon
+                fontSize='small'
+                sx={{
+                  color: 'white',
+                  cursor: 'pointer',
+                  '&:hover': { color: theme => theme.palette.warning.light }
+                }}
+                onClick={toggleOpenNewColumnForm}
+              />
+            </Box>
+          </Box>
+        }
       </Box>
     </SortableContext>
   )
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,7 +3,7 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-// import { mockData } from '~/apis/mock-data'
+import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
@@ -21,8 +21,8 @@ function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={board} />
-      <BoardContent board={board} />
+      <BoardBar board={mockData.board} />
+      <BoardContent board={mockData.board} />
     </Container>
   )
 }
